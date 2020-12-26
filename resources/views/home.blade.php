3ef91d8eaa0dd34c25b9d24b97b1c31a8ebf4c07
shofujimoto0617 <shofujimoto017@gmail.com>
2020-12-26 12:15:40 +0900

    add view_home of Home

---
+++ resources/views/home.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Bookers</title>
+</head>
+<body>
+    <h1>
+        ようこそ <strong>Bookers</strong> へ！
+    </h1>
+    <p>
+        <strong>Bookers</strong>では、さまざまな書籍に関するあなたの意見や
+    </p>
+    <p>印象を共有し交換することができます</p>
+    <p>
+        <a href="">start</a>
+    </p>
+</body>
+</html>
